1533182066eb8e9ed54edf151d8e4446bffcf57d
Nithin-Varma <mnithin1422@gmail.com>
2023-07-02 10:14:58 +0530

    header nad home background

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import PageLayout from '@/components/page-layout';
-import { VStack, Text } from '@chakra-ui/react';
+import { VStack, Text, Box } from '@chakra-ui/react';
+import { BackgroundImage } from '@/components/Background-Image';
 
 
 const IndexPage = () => {
@@ -10,9 +11,12 @@ const IndexPage = () => {
         title='Home'
         description='Discover a starter kit which includes Next.js, Chakra-UI, Framer-Motion in Typescript. You have few components, Internationalization, SEO and more in this template ! Enjoy coding.'
       >
-        <VStack width={'100%'} minH='fit-content'>
-          <Text>Nithin</Text>
-        </VStack>
+        <Box>
+          <BackgroundImage />
+          <VStack width={'100%'} minH='fit-content'>
+            
+          </VStack>
+        </Box>
       </PageLayout>
     </>
   );
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,11 +1,72 @@
+import { ReactNode } from 'react';
 import {
-  Text,
+  Box,
+  Image,
+  Flex,
+  Avatar,
+  Link,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
 } from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 
-const Header = () => {
+const NavLink = ({ children }: { children: ReactNode }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={'md'}
+    _hover={{
+      textDecoration: 'none',
+      bg: useColorModeValue('gray.200', 'gray.700'),
+    }}
+    href={'#'}>
+    {children}
+  </Link>
+);
+
+export default function Header() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <Text>Here is the header</Text>
-  )
-};
+    <>
+      <Box p={8}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <Image
+            boxSize='50px'
+            objectFit='cover'
+            src='/background-logo.png'
+            alt='logo'
+          />
 
-export default Header;
+          <Flex alignItems={'center'}>
+            <Stack direction={'row'} spacing={7} >
+              <Button onClick={toggleColorMode}  >
+                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+              </Button>
+
+              
+              <Button
+               bgGradient="linear(to-l, rgba(255, 255, 37, 1), rgba(83, 219, 255, 1), rgba(255, 85, 243, 1), rgba(83, 219, 255, 1), rgba(255, 0, 255, 1))" 
+               bgClip="text"
+               _hover={{ bgClip: "text" }}
+               fontSize={'2xl'}
+               >
+                Connect Wallet
+               </Button>
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
+}
+// rgba(255, 255, 37, 1), rgba(83, 219, 255, 1), rgba(255, 85, 243, 1), rgba(83, 219, 255, 1), rgba(255, 0, 255, 1)
