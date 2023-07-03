697935937cbd15a2fac7c4fb00b9abe79b553bae
Nithin-Varma <mnithin1422@gmail.com>
2023-07-03 15:27:27 +0530

    metamask connection.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import Layout from '@/components/layout';
-import ClientOnly from '@/components/layout/clientOnly';
 import { ChakraProvider } from '@chakra-ui/react';
 // import '@fontsource/josefin-sans/700.css';
 import { AppProps } from 'next/app';
@@ -8,11 +7,9 @@ import { AppProps } from 'next/app';
 const App = ({ Component, pageProps }: AppProps) => {
   return (
       <ChakraProvider>
-        <ClientOnly>
           <Layout>
             <Component {...pageProps} />
           </Layout>
-        </ClientOnly>
       </ChakraProvider>
   );
 };
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,39 +1,93 @@
-import { ReactNode } from 'react';
-import {
-  Box,
-  Image,
-  Flex,
-  Avatar,
-  Link,
-  HStack,
-  Button,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuDivider,
-  useDisclosure,
-  useColorModeValue,
-  Stack,
-  useColorMode,
-  Center,
-} from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+import { Box, Button, HStack } from '@chakra-ui/react'
+declare var window: any;
 
 export default function Header() {
+  const [address, setAddress] = useState('');
+  const [balance, setBalance] = useState('');
+  const [isConnected, setIsConnected] = useState(false);
+
+  useEffect(() => {
+    async function fetchWalletData() {
+      if (window.ethereum) {
+        try {
+          const provider = new ethers.BrowserProvider(window.ethereum);
+
+          const network = await provider.getNetwork();
+          if (Number(network.chainId) !== 56) {
+            throw new Error('Please configure MetaMask for Binance Smart Chain');
+          }
+
+          const signer = await provider.getSigner();
+          const walletAddress = await signer.getAddress();
+          const walletBalance = await provider.getBalance(walletAddress);
+
+          setAddress(walletAddress);
+          setBalance(ethers.formatEther(walletBalance));
+          setIsConnected(true);
+        } catch (error) {
+          console.error('Error:', error);
+
+        }
+      } else {
+        console.error('MetaMask not detected');
+      }
+    }
+
+    fetchWalletData();
+  }, []);
+
   return (
-    <>
-      <Box p={8} position={'fixed'}
-      >
-              <Button
-               bgGradient="linear(to-l, rgba(255, 255, 37, 1), rgba(83, 219, 255, 1), rgba(255, 85, 243, 1), rgba(83, 219, 255, 1), rgba(255, 0, 255, 1))" 
-               bgClip="text"
-               _hover={{ bgClip: "text" }}
-               fontSize={'2xl'}
-               >
-                Connect Wallet
-               </Button>
+    <HStack spacing={8} padding={'8'}>
+      <Box>
+        {!isConnected ? (
+          <Button
+            bgGradient="linear(to-l, rgba(255, 255, 37, 1), rgba(83, 219, 255, 1), rgba(255, 85, 243, 1), rgba(83, 219, 255, 1), rgba(255, 0, 255, 1))"
+            bgClip="text"
+            _hover={{ bgClip: 'text' }}
+            fontSize={'2xl'}
+            onClick={() => {
+              setIsConnected(true);
+            }}
+          >
+            Connect Wallet
+          </Button>
+        ) : (
+          <HStack>
+            <Button
+            bgGradient="linear(to-l, rgba(255, 255, 37, 1), rgba(83, 219, 255, 1), rgba(255, 85, 243, 1), rgba(83, 219, 255, 1), rgba(255, 0, 255, 1))"
+            bgClip="text"
+            _hover={{ bgClip: 'text' }}
+            fontSize={'2xl'}
+          >
+            {address}
+          </Button>
+          <Button
+            bgGradient="linear(to-l, rgba(255, 255, 37, 1), rgba(83, 219, 255, 1), rgba(255, 85, 243, 1), rgba(83, 219, 255, 1), rgba(255, 0, 255, 1))"
+            bgClip="text"
+            _hover={{ bgClip: 'text' }}
+            fontSize={'2xl'}
+          >
+            {balance}
+          </Button>
+          </HStack>
+        )}
       </Box>
-    </>
+
+      <Box>
+        <Button
+          bgGradient="linear(to-l, rgba(255, 255, 37, 1), rgba(83, 219, 255, 1), rgba(255, 85, 243, 1), rgba(83, 219, 255, 1), rgba(255, 0, 255, 1))"
+          bgClip="text"
+          _hover={{ bgClip: 'text' }}
+          fontSize={'2xl'}
+          onClick={() => {
+            setIsConnected(false);
+          }}
+        >
+          Disconnect Wallet
+        </Button>
+    </Box>
+  </HStack>
   );
 }
-// rgba(255, 255, 37, 1), rgba(83, 219, 255, 1), rgba(255, 85, 243, 1), rgba(83, 219, 255, 1), rgba(255, 0, 255, 1)
